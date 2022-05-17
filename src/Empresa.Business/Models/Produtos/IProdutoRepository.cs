d557b928cacfcfdbdd1f3dd4956b364348f85a7d
Pedro Ferreira <pedrophpvp12@gmail.com>
2022-05-16 22:21:08 -0300

    Implementando repositorio de produto

---
+++ src/Empresa.Business/Models/Produtos/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Empresa.Business.Core.Data;
+using Empresa.Business.Models.Produtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Empresa.Business.Models.Fornecedores
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
