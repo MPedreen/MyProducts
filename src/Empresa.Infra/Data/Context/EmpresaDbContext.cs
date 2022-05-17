27f1e48d048af3c2ae480be7472446b2906a3b7d
Pedro Ferreira <pedrophpvp12@gmail.com>
2022-05-17 20:14:13 -0300

    Instalando entity framework na camada de Infra e criação do dbcontext

---
+++ src/Empresa.Infra/Data/Context/EmpresaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Empresa.Business.Models.Fornecedores;
+using Empresa.Business.Models.Produtos;
+using System.Data.Entity;
+
+namespace Empresa.Infra.Data.Context
+{
+    public class EmpresaDbContext : DbContext
+    {
+        public EmpresaDbContext() : base("DefaultConnection")
+        { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
