94d7a24724bb431b66d6833efe628ff48eba70e8
Pedro Ferreira <pedrophpvp12@gmail.com>
2022-05-16 22:11:21 -0300

    Implementando repositorio de fornecedor e endereco com tasks especificas

---
+++ src/Empresa.Business/Models/Fornecedores/IEnderecoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Empresa.Business.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Empresa.Business.Models.Fornecedores
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
